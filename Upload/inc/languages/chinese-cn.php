3df8b4f740a1b85486bdc0131667e759ea347559
Yu 'noyle' Liu <noyle@yuliu.me>
2023-07-21 18:31:10 +0800

    补充语言包元信息。

--- Upload/inc/languages/chinese-cn.php
+++ Upload/inc/languages/chinese-cn.php
@@ -1,4 +1,21 @@
 <?php
+ /**
+ * MyBB Language Pack File.
+ *
+ * @language  chinese-cn
+ * @link      https://github.com/yuliu/mybb-language-packs-chinese
+ * @author    Yu 'noyle' Liu
+ * @copyright 2023 Yu 'noyle' Liu
+ * @license   GNU Lesser General Public License v3.0
+ *
+ * Friendly Language Name: 简体中文（中国）
+ * Requires MyBB Version:  1831
+ * Language Pack URI:      https://github.com/yuliu/mybb-language-packs-chinese
+ * Language Pack Author:   Yu 'noyle' Liu
+ * Author URI:             https://github.com/yuliu
+ *
+ */
+
 // The friendly name of the language
 $langinfo['name'] = "简体中文（中国）";
 
@@ -7,6 +24,18 @@ $langinfo['author'] = "Yu 'noyle' Liu";
 
 // The language authors website
 $langinfo['website'] = "https://github.com/yuliu/mybb-language-packs-chinese";
+
+// The documentation site link
+//$langinfo['docs_link'] = "";
+
+// The documentation FAQs site link
+//$langinfo['common_issues_link'] = "";
+
+// The support website link
+//$langinfo['support_link'] = "";
+
+// The additional website name
+//$langinfo['additional_name'] = " ";
 
 // Compatible version of MyBB
 $langinfo['version'] = "1831";
